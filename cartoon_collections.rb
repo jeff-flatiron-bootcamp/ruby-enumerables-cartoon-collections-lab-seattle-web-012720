22d4d47f8d6a7c74ad2cba5a4b47f9648d3fd7d9
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-16 21:28:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,8 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
-  array = []
-  array = dwarves.each_with_index{|dwarf,index| p "/#{index+1}." + dwarf }
-  p array
+  
+  dwarves.each_with_index{|dwarf,index| p "/#{index+1}." + dwarf }
+  
 end
 
 def summon_captain_planet# code an argument here
