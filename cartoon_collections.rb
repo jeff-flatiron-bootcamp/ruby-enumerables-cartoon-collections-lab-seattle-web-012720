86d744c51b01d2336f4433ec03c4b609df1e57cc
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-16 21:32:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.map
+  planeteer_calls.map{|call| 
 end
 
 def long_planeteer_calls# code an argument here
