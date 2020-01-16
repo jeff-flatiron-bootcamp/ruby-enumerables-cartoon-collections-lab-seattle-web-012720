b4113bbb76e5e6100f982408771e4db1fd02fb2a
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-16 21:23:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,7 @@
 def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
   array = []
-  dwarves.each_with_index{|dwarf,index| p dwarf}
+  array = dwarves.each_with_index{|dwarf,index| p "\"" + dwarf + "\""}
   
 end
 
