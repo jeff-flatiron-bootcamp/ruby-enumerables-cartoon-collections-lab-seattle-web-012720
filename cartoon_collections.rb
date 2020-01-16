5934731e18a32c0988afd16d1f3ad04254193eeb
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-16 21:37:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ end
 
 def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
-  planeteer_calls.any?{|item| if(item.length > 4)}
+  planeteer_calls.any?{|item| if(item.length > 4) end}
 end
 
 def find_the_cheese# code an argument here
