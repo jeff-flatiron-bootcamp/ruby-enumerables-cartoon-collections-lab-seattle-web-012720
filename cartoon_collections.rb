48e600b34f90883ef7a5df927afb6cc55385edec
Jeff Simon <jeffsbillinfo@gmail.com>
2020-01-16 21:34:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,9 @@ def summon_captain_planet(planeteer_calls)# code an argument here
   planeteer_calls.map{|call| call.capitalize() + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(planeteer_calls)# code an argument here
   # Your code here
+  
 end
 
 def find_the_cheese# code an argument here
